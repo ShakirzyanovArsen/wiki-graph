767b1038e1a59671a73897d9e5c23f7b0d5948aa
DmitriyHozyashev <lorddelivuskraust@gmail.com>
2021-03-04 18:11:17 +0500

    module main correction

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from api import ApiWrapper
+from add_type import RevsWithTypes
 import json
 
 if __name__ == '__main__':
     api_wrapper = ApiWrapper('ru', 'wikipedia')
     revisions = api_wrapper.get_revisions_diffs('Пермский_национальный_исследовательский_политехнический_университет')
+    add_type = RevsWithTypes()
+    result = add_type.add_type_to_revision(revisions)
     with open('data.json', 'w', encoding='utf-8') as f:
-        json.dump(revisions, f, ensure_ascii=False)
+        json.dump(result, f, ensure_ascii=False)
