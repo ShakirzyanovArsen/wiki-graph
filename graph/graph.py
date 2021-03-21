79f6527a74341f80f0e6b25d7ec7ba13b65d1c19
DmitriyHozyashev <lorddelivuskraust@gmail.com>
2021-03-22 01:22:55 +0500

    changed vertices

--- graph/graph.py
+++ graph/graph.py
@@ -1,3 +1,5 @@
+import re
+
 class MakeGraph:
 
     def __init__(self):
@@ -7,6 +9,7 @@ class MakeGraph:
         graph = {'vertices': [], 'edges': []}
         vertices = []
         vertices.append(reviewed_page)
+        links_regex = 'http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
         for dict in revisions:
             lines_diffs_list = dict['lines_diffs']
             for line_numb in range(len(lines_diffs_list)):
@@ -15,9 +18,10 @@ class MakeGraph:
                     edge = {'from': reviewed_page, 'to': reviewed_page, 'weight': line_diffs['type'], 'new': line_diffs['new']}
                     graph['edges'].append(edge)
                 if line_diffs['type'] == 'Ссылка на источник' or line_diffs['type'] == 'Перевод вики-страницы':
-                    edge = {'from': reviewed_page, 'to': line_diffs['new'], 'weight': line_diffs['type']}
+                    vertex = re.search(links_regex, line_diffs['new'], flags=re.IGNORECASE).group()
+                    edge = {'from': reviewed_page, 'to': vertex, 'weight': line_diffs['type']}
                     graph['edges'].append(edge)
-                    vertices.append(line_diffs['new'])
+                    vertices.append(vertex)
         vertices = list(dict.fromkeys(vertices))
         graph['vertices'] = vertices
         return graph
